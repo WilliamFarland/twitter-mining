282005dbaaee9ccc099dcccd937350b7c18cdc6d
WilliamFarland <william.farland@outlook.com>
2023-02-05 12:05:57 -0500

    added some code org changes and more polishing

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import requests  # library for making api calls
-import json  # library for easy json manipulation
 import codecs  # provides access to the internal Python codec registry
 import re  # regex library
 from set_env import *  # to hide api key from code
 import os  # library for interacting w/ os
+import time  # use for keeping track of time to api
+from datetime import datetime  # use for making out file
+
+# Specify output file here
+OUT_FIlE = 'Data/out.txt'
+# Put bearer_token here, you could just use plaintext but be careful about leaking it
+BEARER_TOKEN = DS501_BEARER_TOKEN
+# Put keywords for searching here
+KEYWORDS = "optum lang:en"
+# Set max results here
+MAX_RESULTS = 10
 
 
 class TwitterAPI:
@@ -21,11 +31,12 @@ class TwitterAPI:
         self.max_results = max_results
         # Create search url, based on auth, keywords, and any other parameters
         self.search_url, self.query_params = self.create_url()
+        # Data holder, default empty before calling api
+        self.data = None
 
     def create_auth(self):
         """
         Init auth header
-        :return: auth header in json/dict format
         """
         auth = {"Authorization": f"Bearer {self.bearer_token}"}
         return auth
@@ -55,11 +66,30 @@ class TwitterAPI:
         if response.status_code != 200:
             raise Exception(response.status_code, response.text)
 
-        # Add data to response dict
-        return response.json()
+        self.data = response.json()['data']
+        return
+
+    def create_tweets(self):
+        tweet_data = {}
+        for entries in self.data:
+            id = entries['id']
+            text = entries['text']
+            author_id = entries['author_id']
+            referenced_tweets = entries.get('referenced_tweets', None)
+            if referenced_tweets:
+                referenced_tweets = referenced_tweets[0]['type']
+            else:
+                referenced_tweets = 'regular tweet'
+            new_tweet = Tweet(id, text, author_id, referenced_tweets)
+            tweet_data[id] = new_tweet
+
+        return tweet_data
 
 
 class Tweet:
+    """
+    Nice little storage class for tweets
+    """
     def __init__(self, id, text, author_id, referenced_tweets, reply_user=None):
         # Init Vars
         self.id = id
@@ -70,57 +100,39 @@ class Tweet:
         # Modify vars if needed
         self.text = re.sub(r"[^a-zA-Z0-9@ \t]", "", text)
 
-        # Create out json
-        self.out = json.dumps({
-            'text': self.text,
-            'in_reply_to_user_id': self.in_reply_to_user_id,
-            'author_id': self.author_id,
-
-        })
-
     def __str__(self):
         return f'{self.text}'
 
 
-def main():
-    # Tweet Collection
-    tweet_data = {}
-    # Put keywords here
-    keywords = "healthcare lang:en"
-    # Put bearer_token here, you could just use plaintext but be careful about leaking it
-    bearer_token = DS501_BEARER_TOKEN
-    # Initialize base twitter class
-    twitter = TwitterAPI(bearer_token, keywords, max_results=10)
-    # Try connecting to endpoint
-    json_response = twitter.connect_to_endpoint()
-
-    data = json_response['data']
-    for entries in data:
-        id = entries['id']
-        text = entries['text']
-        author_id = entries['author_id']
-        referenced_tweets = entries.get('referenced_tweets', None)
-        if referenced_tweets:
-            referenced_tweets = referenced_tweets[0]['type']
-        else:
-            referenced_tweets = 'regular tweet'
-        new_tweet = Tweet(id, text, author_id, referenced_tweets)
-        tweet_data[id] = new_tweet
-
+def write_to_file(tweet_data):
+    """
+    Simple func, for writing tweet data to text file
+    """
     # check if file exists, if it doesn't create header line
-    if not os.path.isfile('out.txt'):
-        with codecs.open("out.txt", 'a', 'utf-8') as outfile:
-            outfile.write("key, author id, in reply to user id, tweet type, text\n")
+    if not os.path.isfile(OUT_FIlE):
+        with codecs.open(OUT_FIlE, 'a', 'utf-8') as outfile:
+            outfile.write("key, mined time, author id, tweet type, text\n")
 
-    with codecs.open("out.txt", 'a', 'utf-8') as outfile:
+    with codecs.open(OUT_FIlE, 'a', 'utf-8') as outfile:
         for key in tweet_data.keys():
 
             outfile.write(f'{key}, '
+                          f'{datetime.now()}, '
                           f'{tweet_data[key].author_id}, '
-                          f'{tweet_data[key].in_reply_to_user_id}, '
                           f'{tweet_data[key].referenced_tweets}, '
                           f'{tweet_data[key].text} \n'
                           )
+
+
+def main():
+    # Initialize base twitter class
+    twitter = TwitterAPI(BEARER_TOKEN, KEYWORDS, max_results=MAX_RESULTS)
+    # Try getting data
+    twitter.connect_to_endpoint()
+    # Add data to tweet struct format
+    tweet_data = twitter.create_tweets()
+    # Write data to out file by calling function
+    write_to_file(tweet_data)
 
 
 if __name__ == '__main__':
